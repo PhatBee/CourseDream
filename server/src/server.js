ffe6856c538084fb35a3dde4f176694c37b6a34b
PhatBee <vinhphatst1235@gmail.com>
2025-11-10 23:02:24 +0700

    Adjust database user

--- server/src/server.js
+++ server/src/server.js
@@ -3,21 +3,13 @@ dotenv.config();
 
 import mongoose from "mongoose";
 import app from "./app.js";
+import connectDB from "./config/db.js";
+
 
 const PORT = process.env.PORT || 5000;
-const MONGO_URI = process.env.MONGO_URI;
 
-// Connect Database
-mongoose
-  .connect(MONGO_URI)
-  .then(() => {
-    console.log("✅ Connected to MongoDB");
+connectDB(); // ✅ Connect MongoDB trước khi khởi động server
 
-    app.listen(PORT, () => {
-      console.log(`🚀 Server running on http://localhost:${PORT}`);
-    });
-  })
-  .catch((err) => {
-    console.error("❌ MongoDB connection failed", err);
-    process.exit(1);
-  });
+app.listen(PORT, () => {
+  console.log(`🚀 Server running at http://localhost:${PORT}`);
+});
