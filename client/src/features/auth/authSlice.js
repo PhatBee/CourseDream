450bb7f2e05b9d1e86635379cd4125aeb5f13aa8
PhatBee <vinhphatst1235@gmail.com>
2025-11-21 05:31:25 +0700

    feat: Implement Axios client with automatic token refresh and Redux auth slice for user authentication.

--- client/src/features/auth/authSlice.js
+++ client/src/features/auth/authSlice.js
@@ -7,7 +7,7 @@ const user = JSON.parse(localStorage.getItem('user'));
 const initialState = {
   user: user ? user : null,
   isError: false,
-  // isSuccess: false,
+  isSuccess: false,
   isLoading: false,
   message: '',
   isRegisterSuccess: false, // Cho trang Register
