0590892298a4ebb1f27afbc9afb3fbbf11f43bff
PhatBee <vinhphatst1235@gmail.com>
2025-11-20 22:46:51 +0700

    feat: add user routes for profile update, password change, and instructor role request

--- server/src/modules/user/user.routes.js
+++ server/src/modules/user/user.routes.js
@@ -1,13 +1,15 @@
 import express from 'express';
 import * as userController from './user.controller.js';
 import { verifyToken } from '../../middlewares/auth.middleware.js';
-import { checkRole } from '../../middlewares/role.middleware.js';
+import { checkRole } from '../../middlewares/role.middleware.js'
+import upload from '../../middlewares/upload.middleware.js';;
 
 const router = express.Router();
 
 router.put(
   '/profile',
-  verifyToken, 
+  verifyToken,
+  upload.single('avatar'),
   userController.updateProfile
 );
 
