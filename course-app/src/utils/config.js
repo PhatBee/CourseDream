a3e90ba8f3dae97f8e4e6e5402818bb78759b8bb
Tama <mytamhuynhhmt@gmail.com>
2025-12-16 19:57:39 +0700

    fix

--- course-app/src/utils/config.js
+++ course-app/src/utils/config.js
@@ -1,7 +1,7 @@
 // Thay đổi IP này thành IP LAN của máy bạn (xem bằng ipconfig/ifconfig)
 // Không dùng localhost vì localhost trên điện thoại là chính nó.
 // Lấy trong .env
-export const API_URL = process.env.API_URL;
+export const API_URL = process.env.EXPO_PUBLIC_API_URL;
 
-export const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID; // Lấy từ Google Cloud Console (loại Web Client ID hoạt động tốt với Expo Go Proxy)
-export const FACEBOOK_APP_ID = process.env.FACEBOOK_APP_ID;
+export const GOOGLE_CLIENT_ID = process.env.EXPO_PUBLIC_GOOGLE_CLIENT_ID; // Lấy từ Google Cloud Console (loại Web Client ID hoạt động tốt với Expo Go Proxy)
+export const FACEBOOK_APP_ID = process.env.EXPO_PUBLIC_FACEBOOK_APP_ID;
